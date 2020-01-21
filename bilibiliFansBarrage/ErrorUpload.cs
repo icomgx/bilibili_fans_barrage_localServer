b85d652d4117046977ea36651c96f6acc7992fd2
Sheng Fan <fred-0913@outlook.com>
2020-01-21 19:57:08 +0800

    完善错误上报机制

--- bilibiliFansBarrage/ErrorUpload.cs
+++ bilibiliFansBarrage/ErrorUpload.cs
@@ -1,5 +1,9 @@
 using System;
+using System.IO;
+using System.Net;
+using System.Text;
 using System.Web;
+using System.Windows.Forms;
 
 namespace bilibiliFansBarrage
 {
@@ -22,13 +26,20 @@ namespace bilibiliFansBarrage
         }
 
 
-        public static void ErrorUpload(string Error)
+        public static void UploadError(string Error)
         {
             try
             {
                 HttpGet("http://ft2.club:1088/e=" + System.Web.HttpUtility.UrlEncode(Error));
             }
             catch (Exception) { }
+        }
+        public static void AskUploadError(string Error)
+        {
+            if (MessageBox.Show(Error + "\n\n选择'确定'上报错误给开发者！程序将不会上传您的任何个人信息。", "数据库连接时发生错误：", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                UploadError(Error);
+            }
         }
     }
 }
